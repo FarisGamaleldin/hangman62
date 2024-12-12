20a43f57fe950af41bb4fbcf41ac86ce7383a398
FarisGamaleldin <farisfoads@gmail.com>
2024-12-12 02:51:57 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -5,4 +5,9 @@ print(word_list)
 word = random.choice(word_list)
 print(word)
 guess = input("Enter a single letter: ")
-print("Your guess is:", guess)
+if len(guess) == 1 and guess.isalpha():
+  print("Good guess!")
+else:
+  print("Oops! That is not a valid input:")
+
+
