13e852a15eb0187b64f1c2f0943c009dcb15ac7c
FarisGamaleldin <farisfoads@gmail.com>
2024-12-12 01:45:15 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -2,6 +2,5 @@ import random
 favorite_fruits = [ "mangos", "watermelons", "apples", "grapes", "bananas"]
 word_list = favorite_fruits
 print(word_list)
-
 word = random.choice(word_list)
 print(word)
