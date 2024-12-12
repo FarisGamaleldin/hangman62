0d4f028c3c55bce47c88430177bf5e8d1e79eee6
FarisGamaleldin <farisfoads@gmail.com>
2024-12-12 01:22:46 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,4 +1,4 @@
-favorite_fruits = [ mangos, watermelons, apples, grapes, bananas]
+favorite_fruits = [ "mangos", "watermelons", "apples", "grapes", "bananas2]
 word_list = favorite_fruits
 print(word_list)
 
