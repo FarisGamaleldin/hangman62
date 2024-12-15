d950b854aeb4af446f1f53e047dce8a63d562f5c
Faris Gamaleldin <farisfoads@gmail.com>
2024-12-15 21:32:04 +0000

    first commit

--- milestone_4.py
+++ milestone_4.py
@@ -2,25 +2,13 @@ import random
 
 class Hangman:
     def __init__(self, word_list, num_lives=5):
-        """
-        Initialize the Hangman class with the following attributes:
-        
-        :param word_list: list of words from which a random word will be chosen
-        :param num_lives: number of lives the player starts with (default is 5)
-        """
         self.word_list = word_list
-        self.word = random.choice(word_list)  # Randomly pick a word from the word_list
-        self.word_guessed = ['_' for _ in self.word]  # List representation of the word, initially hidden
-        self.num_letters = len(set(self.word))  # Unique letters in the word to be guessed
-        self.num_lives = num_lives  # Number of lives the player starts with
-        self.list_of_guesses = []  # List to track letters guessed by the player
-
+        self.word = random.choice(word_list)  
+        self.word_guessed = ['_' for _ in self.word] 
+        self.num_letters = len(set(self.word))  
+        self.num_lives = num_lives  
+        self.list_of_guesses = []  
     def check_guess(self, guess):
-        """
-        Check if the guessed letter is in the word.
-        
-        :param guess: The letter guessed by the user
-        """
         guess = guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
@@ -31,7 +19,7 @@ class Hangman:
         else:
             print(f"Sorry, {guess} is not in the word.")
             self.num_lives -= 1
-
+            print(f"You have {self.num_lives} lives remaining.")
     def ask_for_input(self):
         """
         Continuously ask the user to guess a letter and validate the input.
@@ -46,9 +34,14 @@ class Hangman:
                 self.check_guess(guess)
                 self.list_of_guesses.append(guess)
                 break
-
-# Example usage:
 if __name__ == "__main__":
     word_list = ["apple", "banana", "cherry", "mango", "orange"]
     hangman_game = Hangman(word_list)
-    hangman_game.ask_for_input()
+    while hangman_game.num_lives > 0 and hangman_game.num_letters > 0:
+        print(f"Word to guess: {' '.join(hangman_game.word_guessed)}")
+        hangman_game.ask_for_input()
+    
+    if hangman_game.num_letters == 0:
+        print(f"Congratulations! You guessed the word: {''.join(hangman_game.word)}")
+    else:
+        print(f"Game over! The word was: {''.join(hangman_game.word)}")
